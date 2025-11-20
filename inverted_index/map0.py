0e012d965d512bddf25d7e1d1102bc670bf1e5ad
Sara Huston <shuston007@gmail.com>
2025-11-19 19:35:40 -0500

    added map0 and reduce0 to count # of html files. Also added partition.py

--- inverted_index/map0.py
+++ inverted_index/map0.py
@@ -1,1 +1,6 @@
-"""Map 0."""
+#!/usr/bin/env python3
+"""Map 0. Counts the number of documents in the input."""
+import sys
+
+for line in sys.stdin:
+    print(f"{line.strip()}\t1")
--- inverted_index/reduce0.py
+++ inverted_index/reduce0.py
@@ -1,1 +1,28 @@
-"""Reduce 0."""
+#!/usr/bin/env python3
+"""Reduce 0. This could have just been counted normally, but I did map reduce structure in case."""
+import sys
+import itertools
+
+def main():
+    """Divide sorted lines into groups that share a key."""
+    for key, group in itertools.groupby(sys.stdin, keyfunc):
+        if key == "<!DOCTYPE html>":
+            reduce_one_group(key, group)
+
+
+def keyfunc(line):
+    """Return the key from a TAB-delimited key-value pair."""
+    return line.partition("\t")[0]
+
+
+def reduce_one_group(key, group):
+    """Reduce one group."""
+    word_count = 0
+    for line in group:
+        count = line.partition("\t")[2]
+        word_count += int(count)
+    print(word_count)
+
+
+if __name__ == "__main__":
+    main()
